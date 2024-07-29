3c2b8b07a7a3cdb5770cb5b5296b1c2c1ef29a38
Paulo Gabriel Lima de Sousa <paulogabriellima@hotmail.com>
2024-07-28 22:40:13 -0300

    style: início da refatoração do header para mobile-first

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,190 +10,192 @@
 
 
 import styled from "styled-components";
-import { InputText } from 'primereact/inputtext';
 import { default as But } from '../components/buttons/Buttons.jsx';
 import { Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 
+const rem = (pixel) => `${pixel / 16}rem`;
+
 const HeaderContainer = styled.header`
     display: flex;
-    max-width: 1440px;
+    align-items: center;
+    justify-content: center;
     max-height: 192px;
-    width: 1440px;
-    height: 192px;
+    width: 100vw;
+    height: 66.5px;
     background-color: var(--white);
     margin: auto;
-
-        & .bloco{
-        margin: 34px 30px 29px 100px;
+    
+    .organizer{
         display: flex;
-        align-items: center;
         justify-content: space-between;
-        flex-wrap: wrap;
-        max-width: 993px;
-        max-height: 129px;
-        width: 100%;
-        height: 100%;
+        width: min(335px, 89.34vw);
 
-        .digital-logo {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        align-self: flex-start;
-        width: 253px;
-        height: 44px;
-        margin-top: 4px;
-
-        @media (max-width: 768px) {
+        & .digital-logo {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
             width: 138px;
             height: 24px;
-            margin-block-start: 4px;
-        }
-
-        & img {
-            margin: 5px 8px 6px 0;
-            
-            @media (max-width: 768px) {
+            margin-left: 33px;
+    
+            & img {
+                margin: 2.73px 0 3.27px 0;
                 width: 18px;
                 height: 18px;
-                margin: 2.73px 4.36px 3.27px 0;
+    
+                @media (min-width: 768px) {
+                    margin-left: auto;
+                    width: 33px;
+                    height: 33px;
+                    margin: 5px 0 6px 0;
+                }
             }
-        }
-
-        & h1 {
-            margin: 0;
-            font-size: 34px;
-            line-height: 44px;
-            font-weight: 600;
-            letter-spacing: 1px;
-            color: var(--primary);
-
-            @media (max-width: 768px) {
+    
+            & h1 {
+                margin: 0;
                 width: auto;
-                font-size: 18px;
+                font-size: 19.85px;
                 line-height: 24px;
-                letter-spacing: 0.7px;
+                font-weight: 600;
+                letter-spacing: -0.01rem;
+                color: var(--primary);
+    
+                @media (min-width: 768px) {
+                    font-size: 36px;
+                    line-height: 44px;
+                    letter-spacing: 1px;
+                }
+            }
+            @media (min-width: 768px) {
+                height: 44px;
             }
         }
-    }
-
-        & .inp-bloco{
-            display: flex;
-            align-items: center;
-            gap: 48px;
-            align-self: flex-start;
-            position: relative;
-
-            & > div > img {
-                position: absolute;
-                top: 18px;
-                right: 24%
+            & .inp-bloco{
+                display: flex;
+                align-items: center;
+                gap: 48px;
+                align-self: flex-start;
+                position: relative;
+    
+                & > div > img {
+                    position: absolute;
+                    top: 18px;
+                    right: 24%
+                }
+                & .cadastre {
+                    inline-size: 102px;
+                    block-size: 28px;
+                    color: var(--dark-gray-3);
+                    text-decoration: none;
+                }
             }
             
-            & .p-inputtext {
-                display: flex;
-                inline-size: 559px;
-                block-size: 60px;
-                border-radius: 8px;
-                padding: 0 24px 0 24px;
-                background-color: rgba(71, 71, 71, 0.4);
-                color: var(--dark-gray-3);
-                border-color: transparent;
-                position: relative;
-                &::placeholder {
-                    color: var(--dark-gray-3);
+            & nav{
+                & label{
+                    display: flex;
+                    width: 24px;
+                    height: 24px;
+    
+                    &  .menu-sidebar {
+                        display: none;
+                    }
+
+                    & img {
+                        margin: 0;
+                        width: 24px;
+                        height: 24px;
+                    }
                 }
-                
-            }
-            & .cadastre {
-                inline-size: 102px;
-                block-size: 28px;
-                color: var(--dark-gray-3);
-                text-decoration: none;
-            }
-        }
+    
+                & .links {
+                    display: none;
+                    gap: 32px;
+                    margin-block-end: 1px;
+                    align-items: center;
+                    justify-content: center;
+                    align-self: flex-end;
         
-        & .pages {
-            display: flex;
-            gap: 32px;
-            margin-block-end: 1px;
-            align-items: center;
-            justify-content: center;
-            align-self: flex-end;
-
-            & a {
-                @keyframes fade-in {
-                    0% {
-                        background-color: var(--primary);
-                        inline-size: 0%;
+                    & a {
+                        @keyframes fade-in {
+                            0% {
+                                background-color: var(--primary);
+                                inline-size: 0%;
+                            }
+                            100% {
+                                inline-size: 100%;
+                            }
+                        }
+                        
+                        block-size: 29px;
+                        color: var(--dark-gray-3);
+                        transition-duration: 500ms;
+                        text-decoration: none;
+        
+                        &[aria-current="page"] {
+                            color: var(--primary);
+                            transition-duration: 500ms;
+                        }
+        
+                        &:not([aria-current="page"])::after {
+                            content: "";
+                            display: block;
+                            border-radius: 2px;
+                            inline-size: 0%;
+                            block-size: 2px;
+                            margin-block-start: 5px;
+                            background-color: transparent;
+                            transition-duration: 500ms;
+                        }
+        
+                        &[aria-current="page"]::after {
+                            content: "";
+                            display: block;
+                            border-radius: 2px;
+                            inline-size: 100%;
+                            block-size: 2px;
+                            margin-block-start: 5px;
+                            background-color: var(--primary);
+                            transition-duration: 500ms;
+                            animation: fade 500ms 1;
+                        }
                     }
-                    100% {
-                        inline-size: 100%;
-                    }
-                }
-                
-                block-size: 29px;
-                color: var(--dark-gray-3);
-                transition-duration: 500ms;
-                text-decoration: none;
-                &[aria-current="page"] {
-                    color: var(--primary);
-                    transition-duration: 500ms;
-                }
-                &:not([aria-current="page"])::after {
-                    content: "";
-                    display: block;
-                    border-radius: 2px;
-                    inline-size: 0%;
-                    block-size: 2px;
-                    margin-block-start: 5px;
-                    background-color: transparent;
-                    transition-duration: 500ms;
-                }
-                &[aria-current="page"]::after {
-                    content: "";
-                    display: block;
-                    border-radius: 2px;
-                    inline-size: 100%;
-                    block-size: 2px;
-                    margin-block-start: 5px;
-                    background-color: var(--primary);
-                    transition-duration: 500ms;
-                    animation: fade 500ms 1;
-                }
                 }
             }
+    
+        Button {
+            block-size: 40px;
+            margin: 44px 70px 0 0;
         }
 
-    Button {
-        block-size: 40px;
-        margin: 44px 70px 0 0;
-    }
-
-    .carrinho{
-        margin-block-start: 47px;
-        position: relative;
-        & span.p-badge{
+        .search-buy{
             display: flex;
-            position: absolute;
-            inset-block-start: -5px;
-            inset-inline-end: -8px;
-            background-color: var(--error);
-            min-inline-size: 5px;
-            inline-size: 17px;
-            block-size: 17px;
-            color: var(--white);
-            /* font-weight: 700; */
-            border-radius: 8px;
-            justify-content: center;
-            align-items: center;
+            width: 66px;
+            justify-content: space-between;
         }
+    
+        .carrinho{
+            position: relative;
+            & span.p-badge{
+                clip-path: circle(8.5px at center);
+                display: flex;
+                position: absolute;
+                top: -5px;
+                right: -11px;
+                background-color: var(--error);
+                height: 17px;
+                width: 17px;
+                color: var(--white);
+                justify-content: center;
+                align-items: center;
+            }
+        }
+        /* @media (min-inline-size: 370px){
+            .bloco{
+                inline-size: 138px;
+            }
+        } */
     }
-    /* @media (min-inline-size: 370px){
-        .bloco{
-            inline-size: 138px;
-        }
-    } */
 `;
 
 const DigitalLogo = () => {
@@ -207,8 +209,8 @@ const DigitalLogo = () => {
 
 const Input = () => {
     return (
-        <div className="input">
-            <InputText id="i" className="text-small" placeholder="Pesquisar..." />
+        <div className="input-drop">
+            <input type="text" placeholder="Pesquisar produto..." />
         </div>
     )
 }
@@ -225,13 +227,20 @@ const Cadastre = () => {
 
 const Pages = () => {
     return (
-        <>
-            <NavLink to="/" className={whereNavLink} >Home</NavLink>
-            <NavLink to="/batata" className={whereNavLink} >Produtos</NavLink>
-            <NavLink to="/" className={whereNavLink} >Categorias</NavLink>
-            <NavLink to="/" className={whereNavLink} >Meus Pedidos</NavLink>
-            {/* <NavLink to="/POR A END-POINT AQUI" className={whereNavLink} >BATATA</NavLink> */}
-        </>
+        <nav>
+            <label>
+                <input type="checkbox" className="menu-sidebar" />
+                <img src="Menu.svg" />
+            </label>
+
+            <div className="links" >
+                <NavLink to="/" className={whereNavLink} >Home</NavLink>
+                <NavLink to="/batata" className={whereNavLink} >Produtos</NavLink>
+                <NavLink to="/" className={whereNavLink} >Categorias</NavLink>
+                <NavLink to="/" className={whereNavLink} >Meus Pedidos</NavLink>
+                {/* <NavLink to="/POR A END-POINT AQUI" className={whereNavLink} >BATATA</NavLink> */}
+            </div>
+        </nav>
     )
 }
 
@@ -248,26 +257,28 @@ const Button = () => {
     )
 }
 
+const Carrin = () => {
+    return (
+        <div className="carrinho">
+            <img src="Buy.svg" />
+            <span className="p-badge">2</span>
+        </div>
+    )
+
+}
+
 const Header = () => {
     return (
         <HeaderContainer>
-            <div className="bloco">
+            <div className="organizer">
+                <Pages />
                 <DigitalLogo />
-                <div className="inp-bloco">
-                    <div>
-                        <Input />
-                        <img src="Search.svg" />
-                    </div>
-                    <Cadastre />
+                <div className="search-buy">
+                    <label htmlFor="" className="out-search">
+                        <img src="Search.svg" alt="" />
+                    </label>
+                    <Carrin />
                 </div>
-                <nav className="pages">
-                    <Pages />
-                </nav>
-            </div>
-            <Button />
-            <div className="carrinho">
-                <img src="Buy.svg" />
-                <span className="p-badge">2</span>
             </div>
         </HeaderContainer>
     );
