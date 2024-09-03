388691ee2812f862e1c8c287b31cbe7e5f5d7ec7
Adriana Abreu Mota Wutke <adriana7089@DIGITALCOLLEGE.LOCAL>
2024-09-03 12:22:23 -0300

    atualizei métodos de pagamentos

--- src/components/MetodosPagamentos.jsx
+++ src/components/MetodosPagamentos.jsx
@@ -135,7 +135,7 @@ const MetodosPagamentos = () => {
                   onClick={() => handleDelete(cartao.id)}
                 ></i>
                 <ConfirmDialog
-                  className="bg-light-gray-3 drop-shadow-lg text-dark-gray-2 font-bold rounded w-[315px] md:w-[350px] lg:w-[420px] px-4 py-2 lg:py-4 lg:px-4 absolute -translate-y-10 lg:-translate-y-40 lg:right-72"
+                  className="bg-light-gray-3 drop-shadow-lg text-dark-gray-2 font-bold rounded w-[315px] md:w-[350px] lg:w-[420px] px-4 py-2 lg:py-4 lg:px-4 absolute -translate-y-10 lg:translate-y-12 lg:right-90"
                   rejectLabel="Não"
                   acceptLabel="Sim"
                 />
