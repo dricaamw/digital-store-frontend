65024ade91fd0e08d2382b572379e29fde4933ae
Francisco Gleidson Teixeira <03538064393@DIGITALCOLLEGE.LOCAL>
2024-09-10 09:27:31 -0300

    updates

--- src/services/index.js
+++ src/services/index.js
@@ -2,7 +2,7 @@ import { QueryClient } from "@tanstack/react-query";
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL: "http://digital-store-backend-c4an.onrender.com"
+    baseURL: "https://digital-store-backend-c4an.onrender.com"
 })
 
 export const queryClient = new QueryClient();
